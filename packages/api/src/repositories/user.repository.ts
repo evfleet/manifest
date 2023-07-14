b479580a6e58e9726d5dc7411ff3188291ddca0d
Evan Fleet <evfleet@gmail.com>
2023-07-14 15:22:51 -0400

    chore(api): :card_file_box: some basic data modelling

---
+++ packages/api/src/repositories/user.repository.ts
@@ -0,0 +1,16 @@
+/*
+  {
+    "name": string,
+    "email": string;
+    "providers": [
+      {
+        "type": strirg,
+        "value": string
+        "primary": boolean
+      }
+    ],
+    "sessions": [
+      // maybe track sessions to logout from all devices etc
+    ]
+  }
+*/
