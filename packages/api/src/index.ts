edde3daf612e3e9fcee7b9ebe7fa68d95f6ed7af
Evan Fleet <evfleet@gmail.com>
2023-07-14 15:14:57 -0400

    feat(api): :card_file_box: setup mongodb connection

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,15 +1,24 @@
+import "dotenv/config";
 import http from "http";
 
 import app from "./app";
+import database from "./config/database";
 import logger from "./config/logger";
 
-const start = () => {
-  const server = http.createServer(app);
-  const port = 8081;
+const start = async () => {
+  try {
+    const server = http.createServer(app);
+    const port = 8081;
 
-  server.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
-  });
+    await database.connect();
+
+    server.listen(port, () => {
+      logger.info(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    logger.error(err);
+    process.exit(1);
+  }
 };
 
 start();
