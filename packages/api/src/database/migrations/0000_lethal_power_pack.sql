acee02077859257bb9d2fa7f9dc1d79801a6980e
Evan Fleet <evfleet@gmail.com>
2023-08-02 11:56:51 -0400

    feat(api): :card_file_box: add more database tables

--- packages/api/src/database/migrations/0000_lethal_power_pack.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "boards" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"public_id" text NOT NULL,
-	CONSTRAINT "boards_public_id_unique" UNIQUE("public_id")
-);
