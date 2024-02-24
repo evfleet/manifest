6b3ca85a0b2b0712605fb6180bed8a6f4eba77bf
Evan Fleet <evfleet@gmail.com>
2024-02-24 13:42:02 -0500

    feat(api): current user with temporary cors fix

--- packages/api/src/modules/auth/auth.router.ts
+++ packages/api/src/modules/auth/auth.router.ts
@@ -4,5 +4,6 @@ import { authController } from "./auth.controller.js";
 
 export const authRouter = Router();
 
+authRouter.get("/", authController.authenticate);
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
