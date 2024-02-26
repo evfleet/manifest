08a997d5a5c7868f4fd9592128e152395335d078
Evan Fleet <evfleet@gmail.com>
2024-02-26 13:22:33 -0500

    build(root): setup docker for nginx and frontend

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,9 +5,16 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 8081,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8081",
   },
 });
