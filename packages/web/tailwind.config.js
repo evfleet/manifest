694795b1de66701ea838b37231cff485a13ed9af
Evan Fleet <evfleet@gmail.com>
2024-02-28 13:55:38 -0500

    build(web): setup prettier and tailwind plugin

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          1: "#2C3A4B",
+          2: "#394452",
+          3: "#545D69",
+          4: "#6D7580",
+          5: "#858C94",
+          6: "#A5ABB3",
+          7: "#DADEE3",
+          8: "#EBEEF2",
+          9: "#F4F6F9",
+        },
+      },
+    },
   },
   plugins: [],
 };
